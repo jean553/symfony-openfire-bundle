09f749aae3ce101d00a6924ae77b3b71e87e115c
jean <jean@bst>
2015-11-16 15:06:34 +0800

    close upload services

--- Services/DummyOpenfireService.php
+++ Services/DummyOpenfireService.php
@@ -15,6 +15,16 @@ class DummyOpenfireService
     }
 
     /**
+     * @param string $username
+     * @param string $password
+     */
+    public function editUserPassword(
+        $username,
+        $password
+    ) {
+    }
+
+    /**
      * @param string $chatRoomName
      * @param string $ownerName
      */
@@ -37,6 +47,44 @@ class DummyOpenfireService
     }
 
     /**
+     * @param string $chatRoomName
+     * @param string $role
+     * @param string $username
+     */
+    public function deleteUserInChatRoom(
+        $chatRoomName,
+        $role,
+        $username
+    ) {
+    }
+
+    /**
+     * @param string $chatRoomName
+     * @param string $ownerName
+     * @param string $serviceName
+     */
+    public function createChatRoomWithSpecificService(
+        $chatRoomName,
+        $ownerName,
+        $serviceName
+    ) {
+    }
+
+    /**
+     * @param string $chatRoomName
+     * @param string $role
+     * @param string $username
+     * @param string $serviceName
+     */
+    public function addUserInChatRoomWithSpecificService(
+        $chatRoomName,
+        $role,
+        $username,
+        $serviceName
+    ) {
+    }
+
+    /**
      * @param string $username Openfire username for the user
      *
      * @return string
@@ -44,5 +92,23 @@ class DummyOpenfireService
     public function getUserJID($username)
     {
         return $username.'@localhost';
+    }
+
+    /**
+     * @param string $chatRoomName
+     */
+    public function deleteChatRoom(
+        $chatRoomName
+    ) {
+    }
+
+    /**
+     * @param string $chatRoomName
+     * @param string $serviceName
+     */
+    public function deleteChatRoomWithSpecificService(
+        $chatRoomName,
+        $serviceName
+    ) {
     }
 }
